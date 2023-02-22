3c528975d449e63b5fc7beb7d880a32180765444
semimikoh <122251770+YTJ91@users.noreply.github.com>
2023-02-22 13:12:18 +0900

    포트폴리오 메인화면
    완성

---
+++ portfolio/index.js
@@ -0,0 +1,9 @@
+const introText = document.querySelectorAll("span, li");
+window.onload = () => {
+  setTimeout(function () {
+    let timer = 100;
+    introText.forEach((item) => {
+      item.style.animation = `fade 500ms ${(timer += 50)}ms forwards`;
+    });
+  }, 1200);
+};
