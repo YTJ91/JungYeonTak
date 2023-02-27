510b2c8c3af33121eb369a34a70f9da736e4e42a
semimikoh <122251770+YTJ91@users.noreply.github.com>
2023-02-27 18:19:34 +0900

    포트폴리오 D-1
    어렵다 ㅠㅠ

--- portfolio/index.js
+++ portfolio/index.js
@@ -9,11 +9,37 @@ window.onload = () => {
 };
 
 let aboutSection = document.querySelector(".about");
+let mainpage = document.querySelector(".mainpage");
+let skillPage = document.querySelector(".skill");
 
 window.addEventListener("scroll", (e) => {
   // console.log(window.scrollY);
   const rect = aboutSection.getBoundingClientRect();
+
   const opacity = (-rect.top + rect.height / 2) / 300;
 
+  const mainOpacity = (rect.top - rect.height / 2) / 300;
+
+  const skillOpacity = ((rect.top + rect.height / 2) / 300) * -1;
+
+  mainpage.style.opacity = mainOpacity;
   aboutSection.style.opacity = opacity;
+  skillPage.style.opacity = skillOpacity;
+});
+
+const sectionList = document.querySelector("section");
+
+window.addEventListener("scroll", (e) => {
+  let opacity =
+    (Math.cos(
+      ((90 / window.innerHeight) * 4 * window.scrollY * Math.PI) / 100
+    ) +
+      1) /
+    2;
+
+  console.log(opacity);
+
+  sectionList.forEach((section) => {
+    sectionList.querySelector("h2").style.opacity = opacity;
+  });
 });
